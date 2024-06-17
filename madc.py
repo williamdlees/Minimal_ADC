9d83cb4aaeb41e57a87841a317e3e9b95244f82b
yanivm96 <yanivm96@gmail.com>
2024-06-17 13:51:06 +0300

    path fixed

--- madc.py
+++ madc.py
@@ -17,4 +17,4 @@ API_INFORMATION = {"version": API_VERSION,
 #STUDIES_PATH = r'C:\Users\yaniv\Desktop\work\minimal_adc\studies'
 #STUDIES_TO_COPY_PATH = r"C:\Users\yaniv\Desktop\work\to_copy"
 STUDIES_PATH = r'/studies/'
-STUDIES_TO_COPY_PATH = r"/misc/work/sequence_data_store/"
+STUDIES_TO_COPY_PATH = r"/work/sequence_data_store/"
